1f69d4060d7889b09b4d7697425e6d56884f6e7b
Eugenius Kriswinar Adi Cahya <140719881+MBAHSINGO22@users.noreply.github.com>
2025-09-25 18:56:04 +0700

    Add files via upload

---
+++ Atas.java
@@ -0,0 +1,10 @@
+package Modul_7;
+
+public class Atas {
+    Atas() {
+    } // constructor
+
+    public void cetak1() {
+        System.out.println("Ada di class ATAS ..");
+    }
+}
---
+++ Bawah.java
@@ -0,0 +1,13 @@
+package Modul_7;
+
+public class Bawah extends Atas {
+        public void cetak2() {
+        System.out.println("Ada di class BAWAH ..");
+        }
+        public static void main(String[] args) {
+        Bawah bwh = new Bawah();
+        bwh.cetak1();
+        bwh.cetak2();
+        }
+        }
+
